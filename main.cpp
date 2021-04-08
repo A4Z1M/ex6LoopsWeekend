8bfee72a8f9f9b4bc67b708b7ddf08158bd76035
7assan-5alloof <hassankhalloof20@gmail.com>
2021-04-08 12:12:17 +0400

    Write program (still needs fixes)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    for (int i = 1; i < 1000; i++)
+    {
+        int sum = 0;
+        for (int j = 1; j < i; j++)
+        {
+            if (i % j == 0) sum += j;
+        }
+        if (sum == i)
+        {
+            cout << i << " = ";
+            for (int j = 1; j < i; j++) if (i % j == 0) cout << j << " + ";
+            cout << endl;
+        }
+    }
+
     return 0;
 }
