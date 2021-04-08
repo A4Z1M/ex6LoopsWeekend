ff3ea8d8edd14b2612f6649f41f686d6e5330c58
7assan-5alloof <hassankhalloof20@gmail.com>
2021-04-08 14:54:34 +0400

    Comment on code

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int main()
 {
+    // Loop that will run needed procedures
     for (int i = 1; i < 1000; i++)
     {
         int sum = 0;
+
+        // Find sum of proper factors of the number
         for (int j = 1; j < i; j++)
         {
             if (i % j == 0) sum += j;
         }
+
+        // Write desired output if sum equals the original number (if number is perfect)
         if (sum == i)
         {
             cout << i << " = ";
             for (int j = 1; j < i; j++) if (i % j == 0) cout << j << " + ";
-            cout << endl;
+            cout << endl; // Will seperate multiple perfect numbers in different lines
         }
     }
 
